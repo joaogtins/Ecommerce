259eb14a6db052d2b4ca075db1bcaa17e1028634
joaogtins <joaogabrielmlopes@hotmail.com>
2026-07-13 18:17:42 -0300

    Initial commit: e-commerce Prata & Lua - Spring Boot 4.1.0 setup

---
+++ src/main/java/com/prataelua/ecommerce/EcommercePrataLuaApplication.java
@@ -0,0 +1,13 @@
+package com.prataelua.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercePrataLuaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercePrataLuaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prataelua/ecommerce/EcommercePrataLuaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prataelua.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercePrataLuaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
